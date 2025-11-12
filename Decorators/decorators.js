341f4d93cb284479f0e4d3b8cafd53208d901f8c
Ivan Lesnik <ivan.lesnik@moodmedia.hr>
2025-11-12 11:30:00 +0100

    ECMAScriptDecorators

--- Decorators/decorators.js
+++ Decorators/decorators.js
@@ -1,52 +1,47 @@
-function logger<T extends { new (...args: any[]): {} }>(target: T, ctx: ClassDecoratorContext) {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function logger(target, ctx) {
     console.log(target);
     console.log(ctx);
     console.log("Logging...");
     return class extends target {
-        constructor(...args: any[]) {
+        constructor(...args) {
             super(...args);
             console.log("New instance created");
         }
     };
 }
-
-function autobind(target: (...args: any[]) => any, ctx: ClassDecoratorContext) {
-    ctx.addInitializer(function (this: any) {
- //       this[ctx.name] = this[ctx.name].bind(this);
+function autobind(target, ctx) {
+    ctx.addInitializer(function () {
+        //       this[ctx.name] = this[ctx.name].bind(this);
     });
-
-    return function (this: any) {
+    return function () {
         console.log("Autobind decorator applied");
         target.apply(this);
     };
 }
-
 @logger
 class User {
     name = "John Doe";
-
-//    @autobind
+    //    @autobind
     greet() {
         console.log(`Hello, my name is ${this.name}`);
     }
 }
-
 const user = new User();
 const person = new User();
-
-function fieldLogger(target: any, ctx: any) {
+function fieldLogger(target, ctx) {
     console.log("Field decorator applied to:", ctx.name);
     console.log(target);
-
-    return (initialValue: any) => {
+    return (initialValue) => {
         console.log(`Initial value for ${ctx.name}:`, initialValue);
         return initialValue;
     };
 }
-
-function replacer<T>(initValue: T) {
-    return function (target: any, ctx: any) {
+function replacer(initValue) {
+    return function (target, ctx) {
         console.log(`Replacing field ${ctx.name} with value:`, initValue);
         return initValue;
     };
 }
+//# sourceMappingURL=decorators.js.map
