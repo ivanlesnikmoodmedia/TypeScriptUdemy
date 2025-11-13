d4ce2f4f553135dbdf63974c365050372a2c22e2
Ivan Lesnik <ivan.lesnik@moodmedia.hr>
2025-11-13 15:55:10 +0100

    commit

---
+++ Namespace/src/app.ts
@@ -0,0 +1,11 @@
+/// <reference path="components/project-input.ts" />
+/// <reference path="components/project-list.ts" />
+/// <reference path="models/project.ts" />
+/// <reference path="state/project-state.ts" />
+ 
+
+namespace App {
+    new ProjectInput();
+    new ProjectList('active');
+    new ProjectList('finished');
+}
