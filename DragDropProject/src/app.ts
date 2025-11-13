e09f7089aeb3b5acc17f122e662d780e56b8e82c
Ivan Lesnik <ivan.lesnik@moodmedia.hr>
2025-11-13 12:24:42 +0100

    ValidationFunc

--- DragDropProject/src/app.ts
+++ DragDropProject/src/app.ts
@@ -1,14 +1,46 @@
+interface Validatable {
+    value: string | number;
+    required?: boolean;
+    minLength?: number;
+    maxLength?: number;
+    min?: number;
+    max?: number;
+}
+
+function validate (validatableInput: Validatable) {
+    let isValid = true;
+    if(validatableInput.required){
+        isValid = isValid && validatableInput.value.toString().trim().length !==0;
+    }
+    if (validatableInput.minLength != null 
+        && typeof validatableInput.value === 'string')
+        {
+        isValid = isValid && validatableInput.value.length >= validatableInput.minLength;
+        }
+    if (validatableInput.maxLength != null 
+        && typeof validatableInput.value === 'string')
+        {
+        isValid = isValid && validatableInput.value.length <= validatableInput.maxLength;
+        }  
+    if (validatableInput.min != null && typeof validatableInput.value === 'number') {
+        isValid = isValid && validatableInput.value >= validatableInput.min;
+    }
+    if (validatableInput.max != null && typeof validatableInput.value === 'number') {
+        isValid = isValid && validatableInput.value <= validatableInput.max;
+    }
+    return isValid;
+}
 
 function autobind(_: any, _2: string, descriptor: PropertyDescriptor) {
     const originalMethod = descriptor.value;
-    const userInput = descriptor.value;
     const adjDescriptor: PropertyDescriptor = {
         configurable: true,
         get() {
             const boundFn = originalMethod.bind(this);
             return boundFn;
-    }
-   }
+        }
+    };
+    return adjDescriptor;
 }
 
 
@@ -41,15 +73,38 @@ class ProjectInput {
         const enteredDescription = this.descriptionInputElement.value;
         const enteredPeople = this.peopleInputElement.value;
 
-        if (enteredTitle.trim().length === 0 ||
+     /*     if (enteredTitle.trim().length === 0 ||
             enteredDescription.trim().length === 0 ||
             enteredPeople.trim().length === 0) {
-            alert('Invalid input, please try again!');
+            alert('Invalid input, please try again!'); 
             return;
         } else{
 
         return [enteredTitle, enteredDescription, +enteredPeople];
+        } */
+
+        const titleValidatable: Validatable = {
+            value: enteredTitle,
+            required: true
+        };
+        const descriptionValidatable: Validatable = {
+            value: +enteredDescription,
+            required: true,
+            minLength: 1
+        };
+        const peopleValidatable: Validatable = {
+            value: +enteredPeople,
+            required: true,
+            min: 1,
+            max: 5
+        };
+        if (!validate(titleValidatable) ||
+            !validate(descriptionValidatable) ||
+            !validate(peopleValidatable)) {
+            alert('Invalid input, please try again!');
+            return;
         }
+
     }
 
     private clearInputs() {
@@ -61,11 +116,11 @@ class ProjectInput {
     @autobind
     private submitHandler(event: Event) {
         event.preventDefault();
-        this.gatherUserInput();
-        console.log(this.titleInputElement.value);
+        const userInput = this.gatherUserInput();
         if (Array.isArray(userInput)) {
             const [title, desc, people] = userInput;
             console.log(title, desc, people);
+            this.clearInputs();
         }
     }
 
